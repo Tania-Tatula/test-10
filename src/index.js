991bc685c6947c7b1c497fed9197879710fc1aa3
Tania Tatula <tgrogol@gmail.com>
2021-03-23 23:56:48 +0200

    скорочує код шаблону

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import foodCartTpl from './templates/food-card.hbs';
 import menu from './menu.json';
 
+
 const cardsMarkup = document.querySelector('.js-menu');
 
-cardsMarkup.insertAdjacentHTML("beforeend", createFoodCardsMarckup(menu));
 
-function createFoodCardsMarckup(menu){
-    return menu.map(foodCartTpl).join('');
-}
+
+const foodCardsMarckup = foodCartTpl(menu);
+cardsMarkup.insertAdjacentHTML("beforeend", foodCardsMarckup);
