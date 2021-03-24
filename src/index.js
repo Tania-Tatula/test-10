d66c4531616148cc0f058987df641073dae8c7ef
Tania Tatula <tgrogol@gmail.com>
2021-03-24 21:08:20 +0200

    додає функціонал зміни теми по натисканню на повзунок

--- src/index.js
+++ src/index.js
@@ -4,7 +4,42 @@ import menu from './menu.json';
 
 const cardsMarkup = document.querySelector('.js-menu');
 
-
-
 const foodCardsMarckup = foodCartTpl(menu);
 cardsMarkup.insertAdjacentHTML("beforeend", foodCardsMarckup);
+
+
+
+
+
+
+const cheacbox = document.querySelector('#theme-switch-toggle');
+const bodyColor = document.querySelector('body');
+
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+bodyColor.classList.add(Theme.LIGHT);
+
+console.log(bodyColor);
+cheacbox.addEventListener('change', сhangesThemeOfWindow )
+
+
+
+  function сhangesThemeOfWindow(){
+   if(bodyColor.classList.contains(Theme.LIGHT)){
+   
+    bodyColor.classList.remove(Theme.LIGHT);
+    return bodyColor.classList.add(Theme.DARK);
+   }
+
+   bodyColor.classList.remove(Theme.DARK);
+   return bodyColor.classList.add(Theme.LIGHT);
+  }
+
+
+
+  
+  
