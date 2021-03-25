b6dcb93b88a3f32b3e7fa66412e0d47ad8d80202
Tania Tatula <tgrogol@gmail.com>
2021-03-25 14:25:42 +0200

    вирішує проблему зависання повзунка

--- src/js/color-change.js
+++ src/js/color-change.js
@@ -2,18 +2,18 @@ import {variables, Theme} from './variables.js';
 
 
 variables.bodyColor.classList.add(Theme.LIGHT);
-auditLocalStorage();
 
 variables.cheacBox.addEventListener('change', сhangesThemeOfWindow);
 
 
-function auditLocalStorage(){ 
+window.onload = function auditLocalStorage(){ 
     const bodyClass = localStorage.getItem('body-color');
 
      if(bodyClass){
       variables.bodyColor.classList.add(JSON.parse(bodyClass));
 
         if(JSON.parse(bodyClass) == Theme.DARK){
+          variables.bodyColor.classList.replace(Theme.LIGHT, Theme.DARK);
           variables.cheacBox.checked = true;
         }
      }
@@ -24,14 +24,12 @@ function сhangesThemeOfWindow(){
    if(variables.bodyColor.classList.contains(Theme.LIGHT)){
 
     localStorage.setItem('body-color', JSON.stringify(Theme.DARK));
-    variables.cheacBox.checked = true;
 
     return variables.bodyColor.classList.replace(Theme.LIGHT, Theme.DARK);
 
    }
 
-   localStorage.setItem('body-color', JSON.stringify(Theme.LIGHT));
-   variables.cheacBox.checked = false;
+   localStorage.setItem('body-color', JSON.stringify(Theme.LIGHT))
 
    return variables.bodyColor.classList.replace(Theme.DARK, Theme.LIGHT);
 
